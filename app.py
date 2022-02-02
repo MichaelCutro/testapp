172dd7a2bbce27c79ed380beaeedd37685f6e70e
michaelcutro <mikercutro@gmail.com>
2022-02-01 22:42:18 -0500

    Messing around

--- app.py
+++ app.py
@@ -2,9 +2,10 @@ from dash import *
 from testapp import *
 
 app = Dash(__name__)
+server = app.server
 app.layout = html.Div([
     github_info_header(),
-    html.Img(src="assets/wsb.jpeg")
+    html.Img(src="assets/dalio.jpeg")
 ])
 
 if __name__ == '__main__':
