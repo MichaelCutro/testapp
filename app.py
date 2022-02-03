76088693eadf7d9884ec02c1b066742b44f5c08f
michaelcutro <mikercutro@gmail.com>
2022-02-02 20:54:20 -0500

    Done woohoo

--- app.py
+++ app.py
@@ -1,22 +1,34 @@
-from dash import *
+import dash
+from dash.dependencies import Input, Output
+from dash import dcc
+from dash import html
 from testapp import *
 
-app = Dash(__name__)
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 server = app.server
 app.layout = html.Div([
     github_info_header(),
-<<<<<<< HEAD
-    html.Img(src="assets/wsb.jpeg")
-=======
-    html.Img(src="assets/burb.jpeg")
->>>>>>> 01526993d6c0055a458d4633a939e88c0e3cf780
+    html.Div(dcc.Input(id='input-box', type='text')),
+    html.Button('Submit', id='button'),
+    html.Div(id='output-container-button',
+             children='Enter a value and press submit'),
+    html.Img(src="assets/dalio.jpeg", style={'height': '40%', 'width': '40%', 'padding': '2%'})
+
 ])
 
+@app.callback(
+    dash.dependencies.Output('output-container-button', 'children'),
+    [dash.dependencies.Input('button', 'n_clicks')],
+    [dash.dependencies.State('input-box', 'value')])
+def update_output(n_clicks, value):
+    return 'The input value was "{}" and the button has been clicked {} times'.format(
+        value,
+        n_clicks
+    )
+
+
 if __name__ == '__main__':
-    app.run_server(debug=True)
-
-
-
-
-
-
+    # app.run_server(debug=True)
+    app.run_server(host='127.0.0.1', port=3000, debug=True)
